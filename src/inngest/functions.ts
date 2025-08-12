7e85838c37d0c9cc7b15ff7e4897cc52031e41f7
Poorav Kumar Gupta <hipoorav@gmail.com>
2025-08-12 23:58:18 +0530

    ai joba

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
+import { success } from "zod";
 import { inngest } from "./client";
+import {   gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "codeAgent",
+      system: "You are an expert next.js developer.  You write readable and maintainable code you write simple next.js write simple react and next.js snippets.",
+      model: gemini({ model: "gemini-2.0-flash",  }),
+    });
+
+    // Run the agent with an input.  This automatically uses steps
+    // to call your AI model.
+   const { output } = await codeAgent.run(
+  `Write the following snippet: ${event.data.value}`,
 );
+    
+    return {output}
+  }
+);
+
+
