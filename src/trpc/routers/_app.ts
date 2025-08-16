bb5af68f8db75009200d582b73ddc49f16755479
Poorav Kumar Gupta <hipoorav@gmail.com>
2025-08-17 05:10:49 +0530

    Base MessageUI created

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
 import { projectsRouter } from '@/modules/projects/server/procedures';
 import {  createTRPCRouter } from '../init';
+import { messageRouter } from '@/modules/messages/server/procedures';
 export const appRouter = createTRPCRouter({
-  projects:projectsRouter
+  projects:projectsRouter,
+  messages:messageRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
